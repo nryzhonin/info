6e6a8b7a774af52f4e622982af7712639225d9ab
Nikolay <nikolay.ryzhonin@yandex.ru>
2023-06-22 15:07:45 +0200

    Исправление ошибки

--- perfomance_08042020/pf_cache_error_demo.php
+++ perfomance_08042020/pf_cache_error_demo.php
@@ -6,8 +6,8 @@ global $CACHE_MANAGER;
 function getKey($key)
 {
 	$dir = '/tagdir1/';		// Не корректный вариант, удалятся все ключи, при очистке одного по тегу
-	$dir = '/tagdir1/'.$key.'/';	// Не корректный вариант, так как в файловом кеше будет много папок на одном уровне
-	//$dir = '/tagdir1/'.substr(md5($key),2,2)./.$key.'/';	// Правильный вариант
+	$dir = '/tagdir1/' . $key . '/';	// Не корректный вариант, так как в файловом кеше будет много папок на одном уровне
+	//$dir = '/tagdir1/' . substr(md5($key),2,2) . '/' . $key . '/';	// Правильный вариант
 	$cache = Bitrix\Main\Data\Cache::createInstance();
 	if ($cache->initCache(3600, $key, $dir))
 	{
